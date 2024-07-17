b83cefc5c751dcfc0b7cabd813f8961a681f2754
Jesso3 <jesse2.olbrechts@gmail.com>
2024-07-17 13:25:23 +0200

    reset

--- yt2/loadVideos.js
+++
@@ -1,8 +0,0 @@
-let inter = setInterval(() => window.scrollBy(0, 100), 1);
-let goUp;
-
-setTimeout(() => {
-    clearInterval(inter)
-    goUp = setInterval(() => window.scrollBy(0, -100), 1);
-}, 10000);
-setTimeout(() => clearInterval(goUp),20000)
