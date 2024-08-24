fc5016a09340b5c19395920f16d3133652ca411d
Jesso3 <jesse2.olbrechts@gmail.com>
2024-08-25 00:14:14 +0200

    a lot of stuff

--- Web development/portfolio/src/App.jsx
+++ Web development/portfolio/src/App.jsx
@@ -1,7 +1,60 @@
 import "./App.module.css";
 
 function App() {
-  return <>Hello World!</>;
+  return (
+    <div className="app">
+      <div className="item" id="item1"></div>
+      <div className="item" id="item2"></div>
+      <div className="item" id="item3"></div>
+      <div className="item" id="item4"></div>
+      <div className="item" id="item5"></div>
+      <div className="item" id="item6"></div>
+      <div className="item" id="item7"></div>
+      <div className="item" id="item8"></div>
+      <div className="item" id="item9"></div>
+      <div className="item" id="item10"></div>
+      <div className="item" id="item11"></div>
+      <div className="item" id="item12"></div>
+      <div className="item" id="item13"></div>
+      <div className="item" id="item14"></div>
+      <div className="item" id="item15"></div>
+      <div className="item" id="item16"></div>
+      <div className="item" id="item17"></div>
+      <div className="item" id="item18"></div>
+      <div className="item" id="item19"></div>
+      <div className="item" id="item20"></div>
+      <div className="item" id="item21"></div>
+      <div className="item" id="item22"></div>
+      <div className="item" id="item23"></div>
+      <div className="item" id="item24"></div>
+      <div className="item" id="item25"></div>
+      <div className="item" id="item26"></div>
+      <div className="item" id="item27"></div>
+      <div className="item" id="item28"></div>
+      <div className="item" id="item29"></div>
+      <div className="item" id="item30"></div>
+      <div className="item" id="item31"></div>
+      <div className="item" id="item32"></div>
+      <div className="item" id="item33"></div>
+      <div className="item" id="item34"></div>
+      <div className="item" id="item35"></div>
+      <div className="item" id="item36"></div>
+      <div className="item" id="item37"></div>
+      <div className="item" id="item38"></div>
+      <div className="item" id="item39"></div>
+      <div className="item" id="item40"></div>
+      <div className="item" id="item41"></div>
+      <div className="item" id="item42"></div>
+      <div className="item" id="item43"></div>
+      <div className="item" id="item44"></div>
+      <div className="item" id="item45"></div>
+      <div className="item" id="item46"></div>
+      <div className="item" id="item47"></div>
+      <div className="item" id="item48"></div>
+      <div className="item" id="item49"></div>
+      <div className="item" id="item50"></div>
+    </div>
+  );
 }
 
 export default App;
---
+++ cs/triangle.java
@@ -0,0 +1,14 @@
+public class triangle{
+  public static int height = 10;
+  public static void main(String[] args){
+    for(int i = 0; i < height; i++){
+      for(int j = 0; j <= height-i; j++){
+        System.out.print(' ');
+      }
+      for(int k = 0; k < i*2-1; k++){
+        System.out.print('x');
+      }
+      System.out.println();
+    }
+  }
+}
