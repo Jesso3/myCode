f1477023b623f67291079229f727588822e482ad
Jesso3 <jesse2.olbrechts@gmail.com>
2024-10-05 21:33:06 +0200

    stuff

---
+++ Python/test/hi.py
@@ -0,0 +1,4 @@
+if "hello":
+  print("hello")
+
+  print(round(0.1+0.2,2)==0.3)
--- Web development/practice/yt2/getLinks.js
+++ Web development/practice/yt2/getLinks.js
@@ -1,45 +1,63 @@
-let source = Array.from(document.querySelectorAll('.yt-core-image'))
-  .filter(image => {
-    const parentLink = image.closest('a'); 
-    return parentLink && !parentLink.href.includes('shorts');
-  })
-  .map(image => image.src)
-  .filter(src => src !== "");
+let inter = setInterval(() => window.scrollBy(0, 100), 1);
+let totalTime = 15000;
+let goUp;
 
-  let refs = Array.from(document.querySelectorAll('a'))
-  .filter(a => (a.classList.contains('media-item-thumbnail-container') || a.id.includes('thumbnail')) && !a.href.includes('shorts'))
-  .map(a => a.href)
-  .filter((href, index, self) => href !== "" && href !== null && self.indexOf(href) === index);
+setTimeout(() => {
+    clearInterval(inter);
+    document.querySelectorAll('button[aria-label="Show more"]').forEach(e => e.click());
+    goUp = setInterval(() => window.scrollBy(0, -100), 1);
+}, totalTime-5000);
 
-let ids = refs.map(ref => (ref.match(/(?<=v=)[^&]+/g) || [])[0])
-  .filter(id => id !== undefined);
+setTimeout(() => clearInterval(goUp), totalTime);
 
-let titles = Array.from(document.querySelectorAll('span'))
-.filter(a => 
-  a.classList.contains('yt-core-attributed-string') && 
-  a.getAttribute('role') === "text" && 
-  a.parentElement.tagName === 'H3' && 
-  !a.parentElement.getAttribute('aria-label') 
-)
-  .map(a => a.innerHTML)
+setTimeout(() => {
 
-if (titles.length == 0) {
-  titles = Array.from(document.querySelectorAll('yt-formatted-string'))
-    .filter(a => a.id.includes('video-title'))
-    .map(a => a.innerHTML)
-}
+    document.querySelectorAll('button[aria-label="Show more"]').forEach(e => e.click());
 
-let minLength = Math.min(source.length, refs.length, ids.length, titles.length);
+    let source = Array.from(document.querySelectorAll('.yt-core-image'))
+        .filter(image => {
+            const parentLink = image.closest('a');
+            return parentLink && !parentLink.href.includes('shorts');
+        })
+        .map(image => image.src)
+        .filter(src => src !== "");
 
-let combinedObject = {};
+    let refs = Array.from(document.querySelectorAll('a'))
+        .filter(a => 
+            (a.classList.contains('media-item-thumbnail-container') || a.id.includes('thumbnail')) 
+            && !a.href.includes('shorts'))
+        .map(a => a.href)
+        .filter((href, index, self) => href !== "" && href !== null && self.indexOf(href) === index);
 
-for (let i = 0; i < minLength; i++) {
-  combinedObject[i] = {
-    source: source[i],
-    ref: refs[i],
-    id: ids[i],
-    title: titles[i]
-  };
-}
+    let ids = refs.map(ref => (ref.match(/(?<=v=)[^&]+/g) || [])[0])
+        .filter(id => id !== undefined);
 
-console.log(combinedObject);
+    let titles = Array.from(document.querySelectorAll('span'))
+        .filter(a => 
+            a.classList.contains('yt-core-attributed-string') && 
+            a.getAttribute('role') === "text" && 
+            a.parentElement.tagName === 'H3' && 
+            !a.parentElement.getAttribute('aria-label'))
+        .map(a => a.innerHTML);
+
+    if (titles.length == 0) {
+        titles = Array.from(document.querySelectorAll('yt-formatted-string'))
+            .filter(a => a.id.includes('video-title'))
+            .map(a => a.innerHTML);
+    }
+
+    let minLength = Math.min(source.length, refs.length, ids.length, titles.length);
+
+    let combinedObject = {};
+
+    for (let i = 0; i < minLength; i++) {
+        combinedObject[i] = {
+            source: source[i + source.length-minLength],
+            ref: refs[i + refs.length-minLength],
+            id: ids[i + ids.length-minLength],
+            title: titles[i + titles.length-minLength]
+        };
+    }
+
+    console.log(combinedObject);
+}, totalTime);
